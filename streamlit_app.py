7ed74f06a3c16421ec8cebb10a3d2fd76955700c
yannisdinho <43409895+yannisdinho@users.noreply.github.com>
2025-11-28 15:00:30 +0100

    Update streamlit_app.py
    Changed the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!
+    """My Parents New Healthy Diner
     """)
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
